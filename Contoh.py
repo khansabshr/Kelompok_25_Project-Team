b789e66eee83d72a0f14bf5542cf3ac29e0621d2
dendyhalim8 <dendyhalim8@gmail.com>
2023-06-05 12:14:33 +0700

    penambahan contoh

---
+++ Contoh.py
@@ -0,0 +1,28 @@
+import datetime
+
+def postpaid(riwayat):
+    tanggal = datetime.datetime.now()
+    nominal = float(input("Masukkan nominal pembayaran: "))
+    riwayat.append((tanggal, nominal))
+
+def tampilkan_riwayat(riwayat):
+    print("Riwayat Pembayaran:")
+    for pembayaran in riwayat:
+        tanggal = pembayaran[0].strftime("%d-%m-%Y %H:%M:%S")
+        nominal = pembayaran[1]
+        print(f"{tanggal} - Rp{nominal}")
+
+riwayat_pembayaran = []
+
+while True:
+    pilihan = input("Masukkan pilihan (1/2/3): ")
+
+    if pilihan == "1":
+        postpaid(riwayat_pembayaran)
+    elif pilihan == "2":
+        tampilkan_riwayat(riwayat_pembayaran)
+    elif pilihan == "2":
+        print("Terima kasih! Telah menggunakan Tracity")
+        break
+    else:
+        print("Pilihan tidak valid. Silakan coba lagi.")
